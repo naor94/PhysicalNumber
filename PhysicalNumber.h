ff733de6c33dc2ac4d44dd82128c3c520ece3df9
naor94 <44939883+naor94@users.noreply.github.com>
2019-04-04 17:30:49 -0400

    Add files via upload

---
+++ PhysicalNumber.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "Unit.h"
+
+using namespace std;
+
+namespace ariel{
+    class PhysicalNumber;
+}
+
+class  ariel::PhysicalNumber{
+    public:
+    double val;
+    Unit type;
+
+    public:
+    //constructor
+    PhysicalNumber(double value, Unit u);
+
+    //operators
+    const PhysicalNumber operator+(const PhysicalNumber&) const;
+    PhysicalNumber& operator+=(const PhysicalNumber&);
+    //unary +
+    const PhysicalNumber operator+()const;
+    const PhysicalNumber operator-(const PhysicalNumber&) const;
+    PhysicalNumber& operator-=(const PhysicalNumber&);
+    //unary -
+    const PhysicalNumber operator-()const;
+    // 6 comparison operators
+    friend bool operator>(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator<(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator>=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator<=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator!=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator==(const PhysicalNumber&, const PhysicalNumber&);
+
+
+    //prefix
+    PhysicalNumber& operator++();
+    //postfix
+    const PhysicalNumber operator++(int);
+
+    //prefix
+    PhysicalNumber& operator--();
+    //postfix
+    const PhysicalNumber operator--(int);
+    // i/o stream
+    friend ostream& operator<<(ostream& os, const PhysicalNumber& p); 
+    friend istream& operator>>(istream& is, PhysicalNumber& p);
+
+
+    
+};
