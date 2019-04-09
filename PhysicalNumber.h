8b78846c122b0f29b71bdb50d2c2a0284721c30c
naor94 <44939883+naor94@users.noreply.github.com>
2019-04-09 06:33:09 -0400

    Add files via upload

---
+++ PhysicalNumber.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "Unit.h"
+
+using namespace std;
+
+namespace ariel{
+    class PhysicalNumber;
+}
+
+class  ariel::PhysicalNumber{
+    private:
+    const double typeConvertor(const PhysicalNumber& p) const;
+    const bool isSameDeminsion(const PhysicalNumber& p ) const;
+
+
+
+
+
+    public:
+    double val;
+    Unit type;
+
+    //constructor
+    PhysicalNumber(double value, Unit u);
+
+    //operators
+    const PhysicalNumber operator+(const PhysicalNumber&) const;
+    PhysicalNumber& operator+=(const PhysicalNumber&);
+    //unary +
+    const PhysicalNumber operator+()const;
+    const PhysicalNumber operator-(const PhysicalNumber&) const;
+    PhysicalNumber& operator-=(const PhysicalNumber&);
+    //unary -
+    const PhysicalNumber operator-()const;
+    // 6 comparison operators
+    friend bool operator>(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator<(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator>=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator<=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator!=(const PhysicalNumber&, const PhysicalNumber&);
+    friend bool operator==(const PhysicalNumber&, const PhysicalNumber&);
+
+
+    //prefix
+    PhysicalNumber& operator++();
+    //postfix
+    const PhysicalNumber operator++(int);
+
+    //prefix
+    PhysicalNumber& operator--();
+    //postfix
+    const PhysicalNumber operator--(int);
+    // i/o stream
+    friend ostream& operator<<(ostream& os, const PhysicalNumber& p); 
+    friend istream& operator>>(istream& is, PhysicalNumber& p);
+
+    
+    
+};
